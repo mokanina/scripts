5781005afff2a7e710a39728ed05430d8b278a13
mokanina <mokanina@github.com>
2023-01-08 22:39:05 +0100

    pyenv installation fixed

--- mint_setup/run_setup.sh
+++ mint_setup/run_setup.sh
@@ -17,13 +17,18 @@ source ./helpers/setup_docker.sh
 
 # install pyenv
 curl https://pyenv.run | bash
-#git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-#cd ~/.pyenv && src/configure && make -C src
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+## update ~/.bashrc
+echo -e '\n# setup pyenv' >> ~/.bashrc
+echo -e 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo -e 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo -e 'eval "$(pyenv init -)"' >> ~/.bashrc
+## update ~/.profile
+echo -e '\n# setup pyenv' >> ~/.profile
+echo -e 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
+echo -e 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
+echo -e 'eval "$(pyenv init -)"' >> ~/.profile
 
 
 # install poetry
 pip install poetry
-echo 'export PATH="$HOME/.poetry/bin:$PATH"' >> ~/.bashrc
+echo -e 'export PATH="$HOME/.poetry/bin:$PATH"' >> ~/.bashrc
