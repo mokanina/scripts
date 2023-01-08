f851851b808b3c70577eb7fc48185aa346572101
mokanina <mokanina@example.com>
2023-01-08 21:17:39 +0100

    installation of poetry & pyenv added

--- mint_setup/run_setup.sh
+++ mint_setup/run_setup.sh
@@ -13,3 +13,17 @@ xargs sudo apt-get --yes install < packages.txt
 # set up software
 source ./helpers/setup_git.sh
 source ./helpers/setup_docker.sh
+
+
+# install pyenv
+curl https://pyenv.run | bash
+#git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+#cd ~/.pyenv && src/configure && make -C src
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+
+
+# install poetry
+pip install poetry
+echo 'export PATH="$HOME/.poetry/bin:$PATH"' >> ~/.bashrc
